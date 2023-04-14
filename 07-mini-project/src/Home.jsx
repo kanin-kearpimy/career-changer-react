4d6c2069d565ffdacafe671fc71546007489ff23
kanin-kearpimy <kanin.kearpimy@gmail.com>
2023-04-14 10:58:08 +0700

    fix bug of <Home /> role return data

--- 07-mini-project/src/Home.jsx
+++ 07-mini-project/src/Home.jsx
@@ -5,20 +5,18 @@ import Admin from './Admin'
 import User from './User'
 
 const Home = () => {
-    // You have to use getUserRole() to get role of current user
+    // You have to use currentRole to get role of current user
     // role is specify below
     // admin role is 'admin'
     // user role is 'user'
     // 'other' is who didn't login.
-    const { getUserRole } = useContext(Context)
-    // example of getUserRole.
-    // const _role = getUserRole()
+    const { currentRole } = useContext(Context)
+    // example of currentRole.
+    // const _role = currentRole
     // return type is always string.
-    const [role, setRole] = useState()
-
 
     // You have to add condition here according to role.
-    if (role === '?????') {
+    if (currentRole === 'admin') {
         return (
             <Layout>
                 <Admin />
