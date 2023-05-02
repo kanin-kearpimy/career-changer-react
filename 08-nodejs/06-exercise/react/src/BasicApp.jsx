57156c298b5db3632be240e01b59ca26332bd7b4
kanin-kearpimy <kanin.kearpimy@gmail.com>
2023-05-02 15:59:34 +0700

    new exercise

---
+++ 08-nodejs/06-exercise/react/src/BasicApp.jsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { useState } from "react";
+
+function BasicApp() {
+  const [companies, setCompanies] = useState([]);
+  const [name, setName] = useState();
+  const [taxId, setTaxId] = useState();
+
+  const getCompanies = async () => {
+    // 1) create HttpRequest by axios
+    // axios.get(hostname+route)
+    // 2) use async await in axios
+    // 3) set response to companies by useState (setCompanies)
+  };
+
+  const createCompany = async () => {
+    // 0) get name, taxId
+    // 1) create HttpRequest by axios
+    // axios.get(hostname+route, body)
+    // body should be dynamic by state
+    // 2) use async await in axios
+  };
+
+  return (
+    <div>
+      <h1>Basic Application</h1>
+      <button onClick={getUser}>Get User</button>
+      <button onClick={createUser}>Create User</button>
+      <input
+        type="text"
+        placeholder="name"
+        onChange={(event) => setName(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Company ID"
+        onChange={(event) => setTaxId(event.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Company ID</th>
+            <th>Company Name</th>
+            <th>Company Tax ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {companies.map((company) => {
+            return (
+              <tr key={company.companyId}>
+                <td>{company.companyId}</td>
+                <td>{company.name}</td>
+                <td>{company.taxId}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default BasicApp;
--- 08-nodejs/06-exercise/react/src/main.jsx
+++ 08-nodejs/06-exercise/react/src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import BasicApp from "./BasicApp.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<BasicApp />);
